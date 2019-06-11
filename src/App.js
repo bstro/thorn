54e1ef099a75b3ceb0473cf15ce0174b2862c684
Brendan Stromberger <brendanstromberger@gmail.com>
2019-06-10 17:36:25 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactComponent as Logo } from "./assets/dear_thorn_alternate.svg";
+// import Logo from "./assets/dear_thorn.png";
+import Section from "./Section.js";
+import styled from "styled-components";
+import "./App.css";
+
+const colors = {
+  base: "#EFEFEF",
+  yellow: "#FDCF13",
+  pink: "#FF2D55",
+  slate: "#26353D",
+  red: "#FF3B30"
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  height: 100%;
+  min-width: 800px;
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+// const LogoContainer = styled.img`
+//   width: 33%;
+// `;
+
+const section = styled.section`
+  display: grid;
+  justify-items: center;
+  min-height: 470px;
+`;
+
+const sections = {
+  base: styled(section)`
+    align-items: center;
+  `,
+  yellow: styled(section)`
+    background-color: ${colors.yellow};
+    color: ${colors.pink};
+  `,
+  pink: styled(section)`
+    background-color: ${colors.pink};
+    color: ${colors.yellow};
+  `,
+  slate: styled(section)`
+    background-color: ${colors.slate};
+    color: ${colors.yellow};
+  `,
+  red: styled(section)`
+    background-color: ${colors.red};
+    color: "#FFFFFF";
+  `
+};
+
+const sansStack = "Avenir Next, Avenir, Futura, Helvetica Neue, Helvetica";
+
+const SectionTitle = styled.h1`
+  font-family: ${sansStack};
+  font-size: 72px;
+  font-weight: 600;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <sections.base>
+        <Logo />
+      </sections.base>
+
+      <sections.yellow>
+        <SectionTitle>About Me</SectionTitle>
+      </sections.yellow>
+
+      <sections.pink>
+        <SectionTitle>What's Next</SectionTitle>
+      </sections.pink>
+
+      <sections.slate>
+        <SectionTitle>Why Thorn?</SectionTitle>
+      </sections.slate>
+
+      <sections.red>
+        <SectionTitle>Let's talk</SectionTitle>
+      </sections.red>
+    </Wrapper>
   );
 }
 
